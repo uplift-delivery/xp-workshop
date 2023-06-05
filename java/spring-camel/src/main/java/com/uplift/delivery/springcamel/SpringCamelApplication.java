23ba6563999c39648f2d7845cc0f0f6ceda3687c
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2023-06-05 07:02:19 -0500

    adding spring boot with camel

---
+++ java/spring-camel/src/main/java/com/uplift/delivery/springcamel/SpringCamelApplication.java
@@ -0,0 +1,13 @@
+package com.uplift.delivery.springcamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCamelApplication.class, args);
+    }
+
+}
---
+++ java/spring-camel/src/test/java/com/uplift/delivery/springcamel/SpringCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uplift.delivery.springcamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
